c11ad1045c51e7466ed2d007f83ac92b0e822939
Vinicius R <vrodrigues.code@gmail.com>
2024-09-07 14:29:45 -0300

    init: Initialize the project using Spring Initializr

---
+++ src/main/kotlin/com/example/spring_ai_demo/SpringAiDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_ai_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringAiDemoApplication>(*args)
+}
